93ea24b9a80ff2be0b8fbfe6e7c7080993cad486
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2023-07-12 13:36:59 +0200

    Add basic dapp modal styles

--- src/features/Dapp/shared/Modal.tsx
+++ src/features/Dapp/shared/Modal.tsx
@@ -25,17 +25,6 @@ const modalContainer = css`
   align-items: center;
   backdrop-filter: blur(26px);
   background: #06302e80;
-  // radial-gradient(
-  //   57.41% 54.95% at 64.58% 47.64%,
-  //   rgba(27, 97, 94, 0) 0%,
-  //   rgba(27, 97, 94, 0.2) 100%
-  // ),
-  // linear-gradient(
-  //   137deg,
-  //   rgba(26, 94, 91, 0.9) 0%,
-  //   rgba(26, 106, 103, 0) 100%
-  // ),
-  // rgba(6, 48, 46, 0.5);
 `;
 
 export default function Modal({ children }: ModalProps) {
