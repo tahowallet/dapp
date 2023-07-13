024929335f15b7dd086213c5aa67311b04e82628
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2023-07-13 14:52:07 +0200

    Add Button component tests in the Claim modal

--- src/features/Dapp/Claim/ClaimCheck.tsx
+++ src/features/Dapp/Claim/ClaimCheck.tsx
@@ -1,5 +1,6 @@
 import { css } from "linaria";
 import React from "react";
+import Button from "../shared/Button";
 
 const claimCheckContainer = css`
   padding: 40px 106px 72px;
@@ -15,6 +16,8 @@ const claimHeaderContainer = css`
   align-items: center;
 `;
 
+const icon = require("shared/images/social-icons/discord.svg");
+
 export default function ClaimCheck() {
   return (
     <div className={claimCheckContainer}>
@@ -25,7 +28,47 @@ export default function ClaimCheck() {
       </div>
       <div>
         <input placeholder="Address / Ens / Uns..." />
-        <button>Check eligibility</button>
+        <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+          Primary
+          <Button iconSrc={icon} type="primary">
+            Button
+          </Button>
+          <Button iconSrc={icon} type="primary" isDisabled>
+            Button
+          </Button>
+          <Button iconSrc={icon} type="primary" size="large">
+            Button
+          </Button>
+          <Button iconSrc={icon} type="primary" size="large" isDisabled>
+            Button
+          </Button>
+          Secondary
+          <Button iconSrc={icon} type="secondary">
+            Button
+          </Button>
+          <Button iconSrc={icon} type="secondary" isDisabled>
+            Button
+          </Button>
+          <Button iconSrc={icon} type="secondary" size="large">
+            Button
+          </Button>
+          <Button iconSrc={icon} type="secondary" size="large" isDisabled>
+            Button
+          </Button>
+          Tertiary
+          <Button iconSrc={icon} type="tertiary">
+            Button
+          </Button>
+          <Button iconSrc={icon} type="tertiary" isDisabled>
+            Button
+          </Button>
+          <Button iconSrc={icon} type="tertiary" size="large">
+            Button
+          </Button>
+          <Button iconSrc={icon} type="tertiary" size="large" isDisabled>
+            Button
+          </Button>
+        </div>
       </div>
     </div>
   );
