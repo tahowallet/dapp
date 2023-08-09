f84c94188821434f734c7f1a8a271d0fce06b543
hyphenized <28708889+hyphenized@users.noreply.github.com>
2023-08-09 15:29:35 -0500

    Remove konva type re-exports

--- dapp/src/Map/types.ts
+++ dapp/src/Map/types.ts
@@ -1,12 +1,3 @@
-export type { Stage as KonvaStage } from "konva/lib/Stage"
-export type { Node as KonvaNode } from "konva/lib/Node"
-export type { Group as KonvaGroup } from "konva/lib/Group"
-export type { Layer as KonvaLayer } from "konva/lib/Layer"
-export type { Image as KonvaImage } from "konva/lib/shapes/Image"
-export type { Rect as KonvaRect } from "konva/lib/shapes/Rect"
-export type { Vector2d } from "konva/lib/types"
-export type { KonvaEventListener } from "konva/lib/Node"
-
 export type Dimensions = {
   width: number
   height: number
