026e559d4d02dd567cf1466f7f399c74b635173d
hyphenized <28708889+hyphenized@users.noreply.github.com>
2023-08-10 05:16:07 -0500

    Add zone modal selection controls

--- dapp/src/Map/ZoneModal.tsx
+++ dapp/src/Map/ZoneModal.tsx
@@ -11,6 +11,131 @@ import iconCommunity from "../shared/assets/icons/people.svg"
 import iconStar from "../shared/assets/icons/star.svg"
 import walletIcon from "../shared/assets/icons/wallet.svg"
 
+function PrevBtnIcon({
+  onClick,
+  style = {},
+}: {
+  onClick?: () => void
+  style: React.CSSProperties
+}) {
+  return (
+    <svg
+      width={80}
+      height={80}
+      viewBox="0 0 80 80"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      onClick={onClick}
+      style={style}
+    >
+      <rect
+        x={80}
+        y={80}
+        width={80}
+        height={80}
+        rx={40}
+        transform="rotate(-180 80 80)"
+        fill="#E4EEEE"
+        fillOpacity={0.1}
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M38.067 29.933l-8.014 8.015a2.667 2.667 0 000 3.77l8.062 8.063.01.01 1.415-1.414-.011-.01L32.162 41h18.505v-2H31.829l7.652-7.653-1.414-1.414z"
+        fill="currentColor"
+        fillOpacity="var(--fill-opacity)"
+      />
+      <rect
+        className="circle_stroke"
+        x={79.5}
+        y={79.5}
+        width={79}
+        height={79}
+        rx={39.5}
+        transform="rotate(-180 79.5 79.5)"
+        stroke="currentColor"
+        strokeOpacity="var(--stroke-opacity)"
+      />
+      <style jsx>
+        {`
+          svg {
+            color: #e4eeee;
+            --fill-opacity: 0.5;
+            --stroke-opacity: 0.3;
+          }
+          svg * {
+            transition: all 0.3s ease-out;
+          }
+
+          svg:hover {
+            cursor: pointer;
+            color: var(--semantic-success);
+            --fill-opacity: 1;
+            --stroke-opacity: 1;
+          }
+        `}
+      </style>
+    </svg>
+  )
+}
+
+function NextBtnIcon({
+  onClick,
+  style = {},
+}: {
+  onClick?: () => void
+  style: React.CSSProperties
+}) {
+  return (
+    <svg
+      width={80}
+      height={80}
+      viewBox="0 0 80 80"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      onClick={onClick}
+      style={style}
+    >
+      <rect width={80} height={80} rx={40} fill="#E4EEEE" fillOpacity={0.1} />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M41.933 50.067l8.014-8.015a2.667 2.667 0 000-3.77l-8.062-8.063-.01-.01-1.415 1.414.011.01L47.838 39H29.333v2h18.838l-7.652 7.653 1.414 1.414z"
+        fill="currentColor"
+        fillOpacity="var(--fill-opacity)"
+      />
+      <rect
+        x={0.5}
+        y={0.5}
+        width={79}
+        height={79}
+        rx={39.5}
+        stroke="currentColor"
+        strokeOpacity="var(--stroke-opacity)"
+      />
+      <style jsx>
+        {`
+          svg {
+            color: #e4eeee;
+            --fill-opacity: 0.5;
+            --stroke-opacity: 0.3;
+          }
+          svg * {
+            transition: all 0.3s ease-out;
+          }
+
+          svg:hover {
+            cursor: pointer;
+            color: var(--semantic-success);
+            --fill-opacity: 1;
+            --stroke-opacity: 1;
+          }
+        `}
+      </style>
+    </svg>
+  )
+}
+
 const mockData = {
   details:
     "KryptoKeep is the meeting place of explorers that have tried out the Keep Network",
@@ -21,9 +146,13 @@ const mockData = {
 export default function ZoneModal({
   zoneData: zoneId,
   onClose,
+  onNext,
+  onPrev,
 }: {
   zoneData: string
   onClose: () => void
+  onPrev: () => void
+  onNext: () => void
 }) {
   const data = { ...mockData, ...getZoneData(zoneId) }
 
@@ -31,6 +160,26 @@ export default function ZoneModal({
     <>
       <Modal.Container type="fullscreen" onClickOutside={onClose}>
         <Modal.Content>
+          <PrevBtnIcon
+            style={{
+              position: "absolute",
+              top: "68px",
+              left: -80,
+              zIndex: 1,
+              transform: "translateX(-100%)",
+            }}
+            onClick={onPrev}
+          />
+          <NextBtnIcon
+            style={{
+              position: "absolute",
+              top: "68px",
+              right: -80,
+              zIndex: 1,
+              transform: "translateX(100%)",
+            }}
+            onClick={onNext}
+          />
           <div className="modal_content">
             <header className="column">
               <div className="zone_header_bg">
