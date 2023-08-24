bc6a9ddb66d1791b09a288aaa4ce728315525b89
hyphenized <28708889+hyphenized@users.noreply.github.com>
2023-08-24 02:01:12 -0500

    Add declarations for available contracts
    This reads everything from the resulting deployment-info json and set types and definitions through webpack's define plugin

---
+++ dapp/src/deployment-info.d.ts
@@ -0,0 +1,50 @@
+/* eslint-disable */
+// This file is generated automatically by scripts/write-types.ts
+// Do not modify this file manually.
+// Regenerate it by running `yarn write-types`
+
+declare const CONTRACT_TahoDeployer: string
+
+declare const CONTRACT_Taho: string
+
+declare const CONTRACT_TimelockDeployer: string
+
+declare const CONTRACT_TahoGovernorBravoDelegate: string
+
+declare const CONTRACT_TahoGovernorBravoDelegator: string
+
+declare const CONTRACT_ApprovalTarget: string
+
+declare const CONTRACT_VoteWithFriendsDeployer: string
+
+declare const CONTRACT_BalancerDeployer: string
+
+declare const CONTRACT_MerkleFactoryDeployer: string
+
+declare const CONTRACT_TreasuryVesterDeployer: string
+
+declare const CONTRACT_HuntingGroundRegistry: string
+
+declare const CONTRACT_TAHOETHHuntingGroundDeployer: string
+
+declare const CONTRACT_GameParameters: string
+
+declare const CONTRACT_DeFiNodeVeTaho: string
+
+declare const CONTRACT_CreatorsNodeVeTaho: string
+
+declare const CONTRACT_SocialNodeVeTaho: string
+
+declare const CONTRACT_EducateNodeVeTaho: string
+
+declare const CONTRACT_VampireNodeVeTaho: string
+
+declare const CONTRACT_DeFiNodeDeployer: string
+
+declare const CONTRACT_CreatorsNodeDeployer: string
+
+declare const CONTRACT_SocialNodeDeployer: string
+
+declare const CONTRACT_EducateNodeDeployer: string
+
+declare const CONTRACT_VampireNodeDeployer: string
