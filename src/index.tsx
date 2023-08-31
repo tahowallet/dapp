1799b7022e22f9ca0ec0c03e92e2f9f9ffc2c9bf
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2023-08-31 14:02:14 +0200

    Add basic redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { Web3OnboardProvider } from "@web3-onboard/react"
+import { Provider } from "react-redux"
 import Referrals from "./Referrals"
 import {
   EthereumProviderContext,
@@ -13,27 +14,30 @@ import Claim from "./Claim"
 import GlobalStyles from "./GlobalStyles"
 import MapComponent from "./Map"
 import web3Onboard from "./web3Onboard"
+import reduxStore from "./redux"
 
 function DApp() {
   return (
-    <EthereumProviderContext.Provider value={ethereumProvider}>
-      <Web3OnboardProvider web3Onboard={web3Onboard}>
-        <GlobalStyles />
-        <MapComponent />
-        <Router>
-          <Nav />
-          <Switch>
-            <Route path="/claim">
-              <Claim />
-            </Route>
-            <Route path="/referrals">
-              <Referrals />
-            </Route>
-          </Switch>
-          <Footer />
-        </Router>
-      </Web3OnboardProvider>
-    </EthereumProviderContext.Provider>
+    <Provider store={reduxStore}>
+      <EthereumProviderContext.Provider value={ethereumProvider}>
+        <Web3OnboardProvider web3Onboard={web3Onboard}>
+          <GlobalStyles />
+          <MapComponent />
+          <Router>
+            <Nav />
+            <Switch>
+              <Route path="/claim">
+                <Claim />
+              </Route>
+              <Route path="/referrals">
+                <Referrals />
+              </Route>
+            </Switch>
+            <Footer />
+          </Router>
+        </Web3OnboardProvider>
+      </EthereumProviderContext.Provider>
+    </Provider>
   )
 }
 
