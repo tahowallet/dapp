2241d433b5f5b64e03be30f80629a1c7a97a5044
Karolina Kosiorowska <karolina.kosiorowska@tally.cash>
2023-09-05 14:45:28 +0200

    Add description to `encodeUserData` function

--- src/shared/utils/pool.ts
+++ src/shared/utils/pool.ts
@@ -1,5 +1,13 @@
 import { ethers } from "ethers"
 
+/*
+  The userData needs to be encoded to send a request to join the pool.
+  Currently, this data isn't encoded on the BalancerPoolAgent contract side.
+  Therefore, the encoding is done in the following function.
+
+  See: https://docs.balancer.fi/reference/joins-and-exits/pool-joins.html#userdata
+  See: https://github.com/balancer/balancer-v2-monorepo/blob/227683919a7031615c0bc7f144666cdf3883d212/pkg/balancer-js/src/pool-weighted/encoder.ts#L27-L43
+*/
 // eslint-disable-next-line import/prefer-default-export
 export async function encodeUserData(
   lpTokenSupply: bigint,
