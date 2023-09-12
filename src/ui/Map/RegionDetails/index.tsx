fba66b821831aa6fcd4af900cc0814ce74ad75b5
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2023-09-12 16:51:45 +0200

    Add banner for wallet connections to regions modal

--- src/ui/Map/RegionDetails/index.tsx
+++ src/ui/Map/RegionDetails/index.tsx
@@ -1,26 +1,13 @@
 import React from "react"
-import Button from "shared/components/Button"
 import TabPanel from "shared/components/TabPanel"
-import walletIcon from "shared/assets/icons/wallet.svg"
 import Leaderboard from "./Leaderboard"
 import Staking from "./Staking"
+import BannerConnect from "./BannerConnect"
 
 export default function RegionDetails() {
   return (
     <>
-      <div className="connect_hint row">
-        <p className="connect_hint_label">
-          Before joining a region, you need to connect your Taho Wallet
-        </p>
-        <Button
-          iconSrc={walletIcon}
-          type="primary"
-          size="large"
-          iconPosition="left"
-        >
-          Connect Wallet
-        </Button>
-      </div>
+      <BannerConnect />
       <TabPanel
         tabs={[
           {
@@ -36,23 +23,6 @@ export default function RegionDetails() {
           },
         ]}
       />
-      <style jsx>
-        {`
-          .connect_hint {
-            padding: 36px 24px;
-            background-color: var(--primary-p1-40);
-            justify-content: space-between;
-            gap: 24px;
-            margin-bottom: 36px;
-            border-radius: 8px;
-          }
-
-          .connect_hint_label {
-            color: var(--secondary-s1-80);
-            max-width: 288px;
-          }
-        `}
-      </style>
     </>
   )
 }
