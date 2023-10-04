19a987f0083ec2ca04291ea0a47839dda8f32212
Piotr Kacprzyk <ayioerthi@gmail.com>
2023-10-04 11:18:16 +0200

    Review changes

--- src/ui/Island/RealmDetails/Council.tsx
+++ src/ui/Island/RealmDetails/Council.tsx
@@ -4,7 +4,7 @@ import placeholderCouncil from "shared/assets/placeholder_council.png"
 export default function Council() {
   return (
     <>
-      <div className="council_container">
+      <div className="council_container center">
         <img
           src={placeholderCouncil}
           height={373}
@@ -15,28 +15,20 @@ export default function Council() {
       </div>
       <style jsx>{`
         .council_container {
-          display: flex;
           width: 675px;
           height: 382px;
-          flex-shrink: 0;
           padding: 16px 24px 24px 24px;
-          flex-direction: column;
           gap: 14px;
           margin: 24px 0;
           border-radius: 24px;
           background: var(--primary-p1-40);
-          align-items: center;
-          justify-content: center;
         }
         .placeholder_title {
           position: absolute;
           margin: 0 auto;
           color: var(--secondary-s-1100, #e4eeee);
-          font-family: "QuincyCF";
-          font-size: 42px;
-          font-style: normal;
-          font-weight: 500;
-          line-height: 110%; /* 46.2px */
+          font: var(--text-h1);
+          text-align: center;
           letter-spacing: 0.84px;
         }
       `}</style>
