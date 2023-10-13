a1605d7c04a2fb8da5b2ed7ba7f3359be73c36fb
Michał Paczyński <michal.paczynski@thesis.co>
2023-10-13 14:58:42 +0200

    Move `createImageElement` function to `misc.ts`

--- src/shared/utils/helpers.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export function createImageElement(source: string) {
-  const image = new Image()
-  image.src = source
-
-  return image
-}
