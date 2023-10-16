edd42eadb9b56cddbc4b09ad9b0c49e2feedd6f1
Michał Paczyński <michal.paczynski@thesis.co>
2023-10-16 17:18:41 +0200

    Refactor `claim` selectors

--- src/redux-state/selectors.ts
+++ src/redux-state/selectors.ts
@@ -1,16 +1,29 @@
 import { createSelector } from "@reduxjs/toolkit"
 import { IslandState } from "redux-state/slices/island"
+import { ClaimState } from "./slices/claim"
 import { RootState } from "./reducers"
 
-export type IslandSelector<K extends keyof IslandState> = (
+type IslandSelector<K extends keyof IslandState> = (
   state: RootState
 ) => IslandState[K]
 
-export type CreateIslandSelector = <K extends keyof IslandState>(
+type ClaimSelector<K extends keyof ClaimState> = (
+  state: RootState
+) => ClaimState[K]
+
+type CreateIslandSelector = <K extends keyof IslandState>(
   value: K
 ) => IslandSelector<K>
 
+type CreateClaimSelector = <K extends keyof ClaimState>(
+  value: K
+) => ClaimSelector<K>
+
 export const selectIsland = (state: RootState) => state.island
+export const selectClaim = (state: RootState) => state.claim
 
 export const createIslandSelector: CreateIslandSelector = (value) =>
   createSelector(selectIsland, (island) => island[value])
+
+export const createClaimSelector: CreateClaimSelector = (value) =>
+  createSelector(selectClaim, (claim) => claim[value])
