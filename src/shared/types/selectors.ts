bef0d04edb335f2a0e6117305b59ba5d6c6ffb75
Michał Paczyński <michal.paczynski@thesis.co>
2023-10-17 10:53:44 +0200

    Selector types refactor

--- src/shared/types/selectors.ts
+++ src/shared/types/selectors.ts
@@ -3,17 +3,11 @@ import { ClaimState } from "redux-state/slices/claim"
 import { WalletState } from "redux-state/slices/wallet"
 import { RootState } from "redux-state/reducers"
 
-type IslandSelector<K extends keyof IslandState> = (
-  state: RootState
-) => IslandState[K]
+type Selector<T, K extends keyof T> = (state: RootState) => T[K]
 
-type ClaimSelector<K extends keyof ClaimState> = (
-  state: RootState
-) => ClaimState[K]
-
-type WalletSelector<K extends keyof WalletState> = (
-  state: RootState
-) => WalletState[K]
+type IslandSelector<K extends keyof IslandState> = Selector<IslandState, K>
+type ClaimSelector<K extends keyof ClaimState> = Selector<ClaimState, K>
+type WalletSelector<K extends keyof WalletState> = Selector<WalletState, K>
 
 export type CreateIslandSelector = <K extends keyof IslandState>(
   value: K
