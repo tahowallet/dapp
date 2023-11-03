af5f9c0dba9671242d727ae7ebe4c60a67e2cfe4
Michał Paczyński <michal.paczynski@thesis.co>
2023-11-03 13:16:25 +0100

    Created schemas with `zod`

--- reflect/mutators.ts
+++ reflect/mutators.ts
@@ -1,3 +1,22 @@
-export const mutators = {}
+import { WriteTransaction } from "@rocicorp/reflect"
+import { generate } from "@rocicorp/rails"
+import { Cursor, clientStateSchema, getParse } from "./models"
+
+export const {
+  init: initClientState,
+  get: getClientState,
+  mustGet: mustGetClientState,
+  put: putClientState,
+  update: updateClientState,
+} = generate("client-state", getParse(clientStateSchema))
+
+async function setCursor(
+  tx: WriteTransaction,
+  coordinates: Cursor
+): Promise<void> {
+  await updateClientState(tx, { id: tx.clientID, cursor: coordinates })
+}
+
+export const mutators = { setCursor, initClientState }
 
 export type ReflectMutators = typeof mutators
