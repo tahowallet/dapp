a84b03966ee9e5eb2bc80718a144aafebf6102e6
Michał Paczyński <michal.paczynski@thesis.co>
2023-11-08 10:43:57 +0100

    Refactor `DApps` folder

---
+++ src/ui/DApps/DesktopDApp.tsx
@@ -0,0 +1,53 @@
+import React, { ReactNode, useEffect } from "react"
+import { BrowserRouter as Router, useLocation } from "react-router-dom"
+
+import {
+  useBalanceFetch,
+  useConnect,
+  useGameDataFetch,
+  useGameLoadDataFetch,
+  usePopulationFetch,
+  useWallet,
+  useWalletChange,
+  useWalletOnboarding,
+} from "shared/hooks"
+import Onboarding from "ui/Onboarding"
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { usePostHog } from "posthog-js/react"
+import PrivacyPolicy from "../../shared/components/PrivacyPolicy"
+import IslandView from "./IslandView"
+
+function TrackEvents({ children }: { children: ReactNode[] }) {
+  const location = useLocation()
+  const posthog = usePostHog()
+
+  useEffect(() => {
+    posthog?.capture("$pageview", { url: location.pathname })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return children
+}
+
+export default function DesktopDApp() {
+  const { isConnected } = useConnect()
+  const { walletOnboarded } = useWalletOnboarding()
+
+  useWallet()
+  useGameLoadDataFetch()
+  useBalanceFetch()
+  usePopulationFetch()
+  useGameDataFetch()
+  useWalletChange()
+
+  return (
+    <Router>
+      <TrackEvents>
+        {(!walletOnboarded || !isConnected) && <Onboarding />}
+        {walletOnboarded && isConnected && <IslandView />}
+        <PrivacyPolicy />
+      </TrackEvents>
+    </Router>
+  )
+}
