dee6746b2effbca86857be8375d003c2aa828b28
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2023-11-21 14:31:06 +0100

    Hide missing realm icon on Reflect cursor

--- src/ui/Island/Cursor/CursorLabel.tsx
+++ src/ui/Island/Cursor/CursorLabel.tsx
@@ -18,6 +18,7 @@ export default function CursorLabel({
 
   const { name, realmName, stakingRealmColor, cursorTextColor } = userInfo
   const iconKey = realmName?.toLowerCase() as keyof typeof REALM_ICONS
+  const iconSrc = REALM_ICONS[iconKey]
 
   return (
     <>
@@ -34,13 +35,8 @@ export default function CursorLabel({
         >
           {name}
         </div>
-        {realmName && (
-          <Icon
-            src={REALM_ICONS[iconKey]}
-            type="image"
-            height="16px"
-            width="16px"
-          />
+        {realmName && iconSrc && (
+          <Icon src={iconSrc} type="image" height="16px" width="16px" />
         )}
       </div>
       <style jsx>{`
