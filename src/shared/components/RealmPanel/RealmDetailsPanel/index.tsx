d33708209a407a15ec416435e119b1cc46461c61
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2023-11-24 16:13:34 +0100

    Refactor RealmPanel using new Panel component

---
+++ src/shared/components/RealmPanel/RealmDetailsPanel/index.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+import Panel from "shared/components/Panel"
+
+export default function RealmDetailsPanel() {
+  return (
+    <Panel.Container>
+      <Panel.Section>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam culpa
+        consectetur aliquid in! Nisi, nobis numquam dolores voluptatibus facere
+        nam neque iure repudiandae beatae quisquam ipsam tenetur mollitia, velit
+        sit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+        culpa consectetur aliquid in! Nisi, nobis numquam dolores voluptatibus
+        facere nam neque iure repudiandae beatae quisquam ipsam tenetur
+        mollitia, velit sit? Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Aperiam culpa consectetur aliquid in! Nisi, nobis numquam dolores
+        voluptatibus facere nam neque iure repudiandae beatae quisquam ipsam
+        tenetur mollitia, velit sit? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Aperiam culpa consectetur aliquid in! Nisi, nobis
+        numquam dolores voluptatibus facere nam neque iure repudiandae beatae
+        quisquam ipsam tenetur mollitia, velit sit? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Aperiam culpa consectetur aliquid in!
+        Nisi, nobis numquam dolores voluptatibus facere nam neque iure
+        repudiandae beatae quisquam ipsam tenetur mollitia, velit sit? Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Aperiam culpa
+        consectetur aliquid in! Nisi, nobis numquam dolores voluptatibus facere
+        nam neque iure repudiandae beatae quisquam ipsam tenetur mollitia, velit
+        sit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+        culpa consectetur aliquid in! Nisi, nobis numquam dolores voluptatibus
+        facere nam neque iure repudiandae beatae quisquam ipsam tenetur
+        mollitia, velit sit? Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Aperiam culpa consectetur aliquid in! Nisi, nobis numquam dolores
+        voluptatibus facere nam neque iure repudiandae beatae quisquam ipsam
+        tenetur mollitia, velit sit?
+      </Panel.Section>
+      <Panel.Section>Stake/Unstake</Panel.Section>
+      <Panel.Section>Guardians (coming soon)</Panel.Section>
+    </Panel.Container>
+  )
+}
