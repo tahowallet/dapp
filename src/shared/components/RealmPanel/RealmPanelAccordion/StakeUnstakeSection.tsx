17ec98193d406450a7ea41769c79123d0b6adf3c
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2023-11-28 14:32:37 +0100

    Add basic stake and unstake forms to realm panel

--- src/shared/components/RealmPanel/RealmPanelAccordion/StakeUnstakeSection.tsx
+++ src/shared/components/RealmPanel/RealmPanelAccordion/StakeUnstakeSection.tsx
@@ -1,6 +1,69 @@
-import React from "react"
+import React, { useState } from "react"
+import {
+  selectDisplayedRealmVeTokenAddress,
+  selectIsStakingRealmDisplayed,
+  selectStakingRealmAddress,
+  selectTokenBalanceByAddress,
+  useDappSelector,
+} from "redux-state"
+import { TAHO_ADDRESS } from "shared/constants"
+import TabPanel from "shared/components/TabPanel"
+import StakeForm from "ui/Island/RealmDetails/StakingForms/StakeForm"
+import UnstakeForm from "ui/Island/RealmDetails/StakingForms/UnstakeForm"
 import RealmPanelAccordion from "."
 
+function isFormDisabled(
+  balance: bigint,
+  hasStakingRealm: boolean,
+  isStakingRealm: boolean
+) {
+  return balance === 0n || (hasStakingRealm && !isStakingRealm)
+}
+
 export default function StakeUnstakeSection() {
-  return <RealmPanelAccordion title="Stake/Unstake">TODO</RealmPanelAccordion>
+  const [activeTab, setActiveTab] = useState(0)
+
+  const displayedRealmVeTokenAddress = useDappSelector(
+    selectDisplayedRealmVeTokenAddress
+  )
+  const stakingRealmContractAddress = useDappSelector(selectStakingRealmAddress)
+  const isStakingRealm = useDappSelector(selectIsStakingRealmDisplayed)
+  const hasStakingRealm = !!stakingRealmContractAddress
+
+  const tahoBalance = useDappSelector((state) =>
+    selectTokenBalanceByAddress(state, TAHO_ADDRESS)
+  )
+  const veTahoBalance = useDappSelector((state) =>
+    selectTokenBalanceByAddress(state, displayedRealmVeTokenAddress)
+  )
+
+  const disabledStake = isFormDisabled(
+    tahoBalance,
+    hasStakingRealm,
+    isStakingRealm
+  )
+  const disabledUnstake = isFormDisabled(
+    veTahoBalance,
+    hasStakingRealm,
+    isStakingRealm
+  )
+
+  return (
+    <RealmPanelAccordion title="Stake/Unstake">
+      <TabPanel
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        tabs={[
+          {
+            label: "Stake",
+            component: <StakeForm isDisabled={disabledStake} />,
+          },
+          {
+            label: "Unstake",
+            component: <UnstakeForm isDisabled={disabledUnstake} />,
+          },
+        ]}
+      />
+    </RealmPanelAccordion>
+  )
 }
