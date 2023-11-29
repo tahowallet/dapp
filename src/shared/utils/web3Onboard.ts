f7091ec4a39de27f461a173e0ad1b65b941a3abd
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2023-11-29 11:54:07 +0100

    Add Metamask connection option

--- src/shared/utils/web3Onboard.ts
+++ src/shared/utils/web3Onboard.ts
@@ -1,31 +1,47 @@
 import tahoWalletModule from "@web3-onboard/taho"
 import trezorModule from "@web3-onboard/trezor"
 import walletConnectModule from "@web3-onboard/walletconnect"
+import metamaskSDK from "@web3-onboard/metamask"
 import { init } from "@web3-onboard/react"
 import { ARBITRUM_SEPOLIA } from "shared/constants"
+
+const SUBSACAPE_NAME = "Subscape"
+const SUBSACAPE_URL = "https://app.taho.xyz"
+const SUBSACAPE_ICON =
+  "https://raw.githubusercontent.com/tahowallet/taho.xyz/29a091abf919b5cfcf511fd10c41d73490ce4f23/src/shared/favicon.svg"
+
+const chains = [ARBITRUM_SEPOLIA]
+// TODO: decide what metadata should look like
+const appMetadata = {
+  name: SUBSACAPE_NAME,
+  description: "Subscape dapp",
+  icon: SUBSACAPE_ICON,
+}
 
 const walletsSetup = {
   taho: tahoWalletModule(),
   trezor: trezorModule({
     // TODO: use proper email and url
     email: "doggos@taho.xyz",
-    appUrl: "https://taho.xyz",
+    appUrl: SUBSACAPE_URL,
   }),
   walletConnect: walletConnectModule({
     projectId: process.env.WALLET_CONNECT_ID ?? "",
     requiredChains: [parseInt(ARBITRUM_SEPOLIA.id, 16)],
   }),
+  metamask: metamaskSDK({
+    options: {
+      extensionOnly: false,
+      dappMetadata: {
+        name: SUBSACAPE_NAME,
+        url: SUBSACAPE_URL,
+        base64Icon: SUBSACAPE_ICON,
+      },
+    },
+  }),
 }
 
-const wallets = [walletsSetup.taho]
-
-const chains = [ARBITRUM_SEPOLIA]
-// TODO: decide what metadata should look like
-const appMetadata = {
-  name: "Taho Dapp",
-  description: "Taho Dapp",
-  icon: "https://raw.githubusercontent.com/tahowallet/taho.xyz/29a091abf919b5cfcf511fd10c41d73490ce4f23/src/shared/favicon.svg",
-}
+const wallets = [walletsSetup.taho, walletsSetup.metamask]
 
 const web3Onboard = init({
   wallets,
