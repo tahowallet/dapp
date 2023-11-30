bd686c9f35a5168ac73e99ff262724c4d3e7ef23
Michał Paczyński <michal.paczynski@thesis.co>
2023-11-30 11:26:15 +0100

    Display default avatar on map when `video/mp4` type is detected

--- src/ui/Island/IslandRealmsDetails/RealmPin.tsx
+++ src/ui/Island/IslandRealmsDetails/RealmPin.tsx
@@ -10,7 +10,8 @@ import {
 } from "redux-state"
 import { FIGMA_FACTOR, getRealmMapData } from "shared/constants"
 import { getPinShift } from "shared/utils"
-import KonvaVideo from "shared/components/KonvaVideo"
+// import KonvaVideo from "shared/components/KonvaVideo"
+import portraitImg from "shared/assets/portrait.png"
 
 type RealmPinAvatarProps = {
   x: number
@@ -22,23 +23,25 @@ function RealmPinAvatar({ x, y }: RealmPinAvatarProps) {
   const avatarType = useDappSelector(selectWalletAvatarType)
 
   const [avatarImage] = useImage(avatar)
+  const [portrait] = useImage(portraitImg)
 
-  if (avatarType === "video/mp4") {
-    return (
-      <KonvaVideo
-        src={avatar}
-        x={x + 32}
-        y={y + 28}
-        height={58 * FIGMA_FACTOR.Y}
-        width={58 * FIGMA_FACTOR.X}
-        videoProps={{ cornerRadius: 100 }}
-      />
-    )
-  }
+  // TODO: implement video avatar support
+  // if (avatarType === "video/mp4") {
+  //   return (
+  //     <KonvaVideo
+  //       src={avatar}
+  //       x={x + 32}
+  //       y={y + 28}
+  //       height={58 * FIGMA_FACTOR.Y}
+  //       width={58 * FIGMA_FACTOR.X}
+  //       videoProps={{ cornerRadius: 100 }}
+  //     />
+  //   )
+  // }
 
   return (
     <Image
-      image={avatarImage}
+      image={avatarType === "video/mp4" ? portrait : avatarImage}
       x={x + 32}
       y={y + 28}
       height={58 * FIGMA_FACTOR.Y}
