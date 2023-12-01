7d873a5cd2024430a1d565065714f98de8383c5b
Michał Paczyński <michal.paczynski@thesis.co>
2023-12-01 12:22:58 +0100

    Handle `video.play` promise

--- src/shared/components/KonvaVideo.tsx
+++ src/shared/components/KonvaVideo.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useMemo, useRef, useState } from "react"
+import React, { useEffect, useMemo, useRef } from "react"
 import Konva from "konva"
 import { Image } from "react-konva"
 import { ImageConfig } from "konva/lib/shapes/Image"
-import useImage from "use-image"
 
 type KonvaVideoProps = {
   x: number
@@ -12,26 +11,6 @@ type KonvaVideoProps = {
   src: string
   loop?: boolean
   videoProps?: Partial<ImageConfig>
-}
-
-function extractFirstVideoFrame(
-  video: HTMLVideoElement,
-  width: number,
-  height: number
-) {
-  const canvas = document.createElement("canvas")
-
-  canvas.width = width
-  canvas.height = height
-
-  const canvasCtx = canvas.getContext("2d")
-
-  canvasCtx?.drawImage(video, 0, 0, canvas.width, canvas.height)
-  const firstFrame = canvas.toDataURL()
-
-  canvas.remove()
-
-  return firstFrame
 }
 
 // Source: https://codesandbox.io/p/sandbox/react-konva-video-on-canvas-oygvf?file=%2Fsrc%2Findex.js%3A22%2C31
@@ -45,7 +24,6 @@ export default function KonvaVideo({
   videoProps,
 }: KonvaVideoProps) {
   const imageRef = useRef<Konva.Image>(null)
-  const [videoPaused, setVideoPaused] = useState(true)
 
   const videoElement = useMemo(() => {
     const element = document.createElement("video")
@@ -58,33 +36,32 @@ export default function KonvaVideo({
   useEffect(() => {
     if (!imageRef.current) return () => {}
 
-    const handleVideoPLay = () => {
-      videoElement.play()
-      setVideoPaused(false)
+    // Source: https://stackoverflow.com/a/68128950
+    const handleVideoPLay = async () => {
+      const promise = videoElement.play()
+
+      if (promise !== undefined) {
+        promise.catch(() => {
+          videoElement.muted = true
+          videoElement.play()
+        })
+      }
     }
 
-    document.addEventListener("click", handleVideoPLay)
-
+    handleVideoPLay()
     const imageLayer = imageRef.current.getLayer()
 
     // use Konva.Animation to redraw a layer
     const animation = new Konva.Animation(() => {}, imageLayer)
     animation.start()
 
-    return () => {
-      document.removeEventListener("click", handleVideoPLay)
-      animation.stop()
-    }
+    return () => animation.stop()
   }, [videoElement, imageRef])
-
-  const [firstFrame] = useImage(
-    extractFirstVideoFrame(videoElement, width, height)
-  )
 
   return (
     <Image
       ref={imageRef}
-      image={videoPaused ? firstFrame : videoElement}
+      image={videoElement}
       x={x}
       y={y}
       width={width}
