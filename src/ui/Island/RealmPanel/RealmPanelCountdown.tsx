572a6844cdf639c441f0726ff79fd2ec525e7dc8
Michał Paczyński <michal.paczynski@thesis.co>
2023-12-11 11:37:52 +0100

    Change XP countdown text

--- src/ui/Island/RealmPanel/RealmPanelCountdown.tsx
+++ src/ui/Island/RealmPanel/RealmPanelCountdown.tsx
@@ -5,9 +5,7 @@ import {
   selectWeekEndDate,
   useDappSelector,
 } from "redux-state"
-import { getNextSelectedWeekDay, getTimeRemaining } from "shared/utils"
-import xpBoostIcon from "shared/assets/icons/xp-boost.svg"
-import Icon from "shared/components/Media/Icon"
+// import { getNextSelectedWeekDay, getTimeRemaining } from "shared/utils"
 
 export default function RealmPanelCountdown() {
   const seasonWeek = useDappSelector(selectSeasonWeek)
@@ -17,19 +15,12 @@ export default function RealmPanelCountdown() {
 
   if (!weekEndDate) return null
 
-  const nextDropTimestamp = getNextSelectedWeekDay(4, 17) // 17:00 UTC time
-  const timeRemaining = getTimeRemaining(nextDropTimestamp)
+  // const nextDropTimestamp = getNextSelectedWeekDay(4, 17) // 17:00 UTC time
+  // const timeRemaining = getTimeRemaining(nextDropTimestamp)
 
   return (
     <>
       <div className="countdown row">
-        <Icon
-          type="image"
-          src={xpBoostIcon}
-          width="125px"
-          height="64px"
-          style={{ marginTop: "10px" }}
-        />
         <div className="column">
           <div className="week">
             Week {seasonWeek}{" "}
@@ -37,7 +28,10 @@ export default function RealmPanelCountdown() {
               / {seasonDuration}
             </span>
           </div>
-          <div className="time_remaining">{timeRemaining}</div>
+          {/* <div className="time_remaining">{timeRemaining}</div> */}
+          <div className="time_remaining">
+            Beta is over, claim XP till Dec 18 2023
+          </div>
         </div>
       </div>
       <style jsx>{`
