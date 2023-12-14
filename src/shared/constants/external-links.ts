b3024e731b34792be4d40494d2470967a282f7af
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2023-12-14 11:00:46 +0100

    Update Galxe campaign link

--- src/shared/constants/external-links.ts
+++ src/shared/constants/external-links.ts
@@ -9,5 +9,5 @@ export default {
     "https://support.brave.com/hc/en-us/articles/360023646212-How-do-I-configure-global-and-site-specific-Shields-settings-",
   FEEDBACK: "https://tahowallet.typeform.com/subscapebeta",
   PRIVACY_POLICY: "https://taho.xyz/privacy",
-  GALXE_NFT: "https://galxe.com/taho",
+  GALXE_NFT: "https://galxe.com/taho/campaign/GC17xttozQ",
 }
